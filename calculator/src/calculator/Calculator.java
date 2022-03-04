0fceaa5e6a9687ca183ef90ee5744a001dd963d6
LydiaStephensonGithub <lydiamariastephenson@gmail.com>
2022-03-04 16:23:30 +0000

    calculator

---
+++ calculator/src/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package calculator;
+
+public class Calculator {
+	public static int addition(int n1, int n2) {
+		return n1 + n2;
+	}
+	
+	public static int subtraction(int n1, int n2) {
+		return n1 - n2;
+	}
+	
+	public static int multiplication(int n1, int n2) {
+		return n1 * n2;
+	}
+	
+	public static double division(double n1, double n2) {
+		return n1 / n2;
+	}
+}
---
+++ calculator/src/runner/Runner.java
@@ -0,0 +1,16 @@
+package runner;
+
+import calculator.Calculator;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Calculator.addition(55, 45));
+		System.out.println(Calculator.subtraction(267, 167));
+		System.out.println(Calculator.multiplication(10, 10));
+		System.out.println(Calculator.division(500, 5));
+
+	}
+
+}
